ec0940d3f9acd0c0f29d0c6ab34c90a41273e86e
Nikhil Ben Kuruvilla <nikkibk@gmail.com>
2010-12-05 07:23:39 +0530

    Added new example for displaying multiplication result from an exe file

---
+++ Multiply/index.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ *      index.php
+ *      
+ *      Copyright 2010 Nikhil Ben Kuruvilla <me@nikhilben.com>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
+ */
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>Multiply</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 0.19.1" />
+</head>
+
+<body>
+	<form action="http://localhost/cgi-bin/result.exe">
+<div><label>Multiplicand 1: <input name="m" size="5"></label></div>
+<div><label>Multiplicand 2: <input name="n" size="5"></label></div>
+<div><input type="submit" value="Multiply!"></div>
+</form>
+</body>
+
+</html>
